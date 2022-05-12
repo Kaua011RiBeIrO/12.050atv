f4ab5b0e230b3e604a03e59c681b8c8a4c97863d
Kaua011RiBeIrO <102737682+Kaua011RiBeIrO@users.noreply.github.com>
2022-05-12 11:13:24 -0300

    Add files via upload

---
+++ atv.js
@@ -0,0 +1,26 @@
+atv01
+
+let idade = prompt('Digite sua Idade')
+if (idade > 16){
+    alert('Voce ja pode votar')
+}else{
+    alert('Voce ainda nao pode votar')
+}
+
+atv02
+
+let senha = parseInt (prompt('Digite sua senha'))
+if (senha == 1234){
+    alert('ACESSO PERMITIDO')
+}else{
+    alert('ACESSO NEGADO')
+}
+
+atv03
+
+let periodo = prompt('Digite seu periodo')
+if (periodo == 'm') alert('Bom dia');
+else if(periodo == 'v') alert('Bom tarde');
+else if (periodo == 'n') alert('Boa noite');
+else final = alert('Indefinido'); 
+
